56beca92feff5cc5035b8de1cf2212bff1bc0469
Jim <jimbai2@gmail.com>
2013-08-22 21:59:55 +0000

    updated web.js to include index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require('fs');
+  //response.send('Hello World2!');
+  indexBuffer = fs.readFileSync('index.html');
+  response.send(indexBuffer.toString());
+  
 });
 
 var port = process.env.PORT || 5000;
